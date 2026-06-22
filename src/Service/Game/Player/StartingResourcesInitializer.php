ff7c6cc36376fbc62366a7332a32c7baae12c240
Kevin Zugasti <bearded-developer@protonmail.com>
2026-06-22 16:16:05 +0200

    Fix: modify call for enum and twig

--- src/Service/Game/Player/StartingResourcesInitializer.php
+++ src/Service/Game/Player/StartingResourcesInitializer.php
@@ -3,6 +3,7 @@
 namespace App\Service\Game\Player;
 
 use App\Entity\Player;
+use App\Enum\ResourceCode;
 use App\Repository\PlayerInventoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -21,13 +22,18 @@ class StartingResourcesInitializer
         }
 
         // Quantités de départ
-        $startingResources = ['iron' => 100, 'iron_ingot' => 30, 'oil' => 100, 'latex' => 30,];
+        $startingResources = [
+            ResourceCode::IRON->value       => 100,
+            ResourceCode::IRON_INGOT->value => 30,
+            ResourceCode::OIL->value        => 100,
+            ResourceCode::LATEX->value      => 30,
+        ];
 
         foreach ($this->playerInventoryRepository->findByPlayer($player) as $inventory) {
 
-            $code = $inventory->getResourceType()->getCode();
+            $code = $inventory->getResourceType()->getCode()?->value;
 
-            if (isset($startingResources[$code])) {
+            if ($code !== null && isset($startingResources[$code])) {
                 $inventory->setQuantity($startingResources[$code]);
             }
         }
