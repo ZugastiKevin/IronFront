adfd7e8d129cfb802d188ada316b1dab3a0171eb
Kevin Zugasti <bearded-developer@protonmail.com>
2026-06-25 19:55:17 +0200

    feat: add fog_mode column to game table and remove foreign key constraint from resource_image

--- migrations/Version20260625172225.php
+++ migrations/Version20260625172225.php
@@ -21,13 +21,11 @@ final class Version20260625172225 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE game ADD fog_mode VARCHAR(255) NOT NULL');
-        $this->addSql('ALTER TABLE resource_image ADD CONSTRAINT FK_EE867E6D98EC6B7B FOREIGN KEY (resource_type_id) REFERENCES resource_type (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE game DROP fog_mode');
-        $this->addSql('ALTER TABLE resource_image DROP FOREIGN KEY FK_EE867E6D98EC6B7B');
     }
 }
