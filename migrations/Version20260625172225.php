8183d1474b7899e5fb6e3df4c3e338362975de33
Kevin Zugasti <bearded-developer@protonmail.com>
2026-06-25 20:00:12 +0200

    refactor: remove auto-generated comments and update fog_mode migration SQL statements

--- migrations/Version20260625172225.php
+++ migrations/Version20260625172225.php
@@ -19,13 +19,9 @@ final class Version20260625172225 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE IF EXISTS game ADD fog_mode VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE IF EXISTS game DROP fog_mode');
     }
 }
